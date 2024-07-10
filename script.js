d1db8e50e204a391f33600ae54df243c9f835a93
jarviswynn123 <jmwynn11998@gmail.com>
2024-07-09 21:14:14 -0400

    assignment done

--- script.js
+++ script.js
@@ -92,9 +92,189 @@ Complete the exercise in the space below:
 */
 
 function greetUser(name, timeOfDay) {
-    return "Good " + timeOfDay + "," + name + "!";
+    return "Good " + timeOfDay + "," + " " + name + "!";
 }
 
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
+        return num2;
+    } else {
+        return num3
+    }
+}
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(billAmount, tipPercentage) {
+    return billAmount * (tipPercentage * .01);
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+function convertTemperature(temperature, scale) {
+    if (scale == "F") {
+        return (temperature - 32) / 1.8;
+    }
+    if (scale == "C") {
+        return temperature * (9 / 5) + 32
+    }
+    return null;
+}
+
+console.log('Exercise 8 Result:', convertTemperature(32, "F"));
+
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+function basicCalculator(num1, num2, operation) {
+    if (operation == "add") {
+        return num1 + num2;
+    }
+    if (operation == "subtract") {
+        return num1 - num2;
+    }
+    if (operation == "multiply") {
+        return num1 * num2;
+    }
+    if (operation == "divide") {
+        return num1 / num2;
+    }
+    return null;
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(score) {
+    if (score > 89) {
+        return "A"
+    }
+    if (score > 79 && score < 90) {
+        return "B"
+    }
+    if (score > 69 && score < 80) {
+        return "C"
+    }
+    if (score > 59 && score < 70) {
+        return "D"
+    }
+    if (score < 60) {
+        return "F"
+    }
+    return -1
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+function createUsername(firstName, lastName) {
+    return firstName.substring(0, 3) + lastName.substring(0, 3) + (firstName.length + lastName.length)
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs() {
+    
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
 
